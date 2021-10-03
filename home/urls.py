26f655ae4e2c482fad58285271718ca9cb4af703
daikoshg@gmail.com <daikoshg@gmail.com>
2021-10-03 18:00:10 +0900

    add projects links

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import IndexView, ANNView
+from .views import IndexView, ProjectsView, AboutusView, ContactView
+from .views import TimelineView, AnnView, WankoView, Ann0View, NyankoView
+
 
 urlpatterns = [
     path('', IndexView.as_view()),
-    path('ann/', ANNView.as_view()),
+    path('projects/', ProjectsView.as_view()),
+    path('aboutus/', AboutusView.as_view()),
+    path('contact/', ContactView.as_view()),
+    path('projects/timeline/', TimelineView.as_view()),
+    path('projects/ann/', AnnView.as_view()),
+    path('projects/wanko/', WankoView.as_view()),
+    path('projects/ann0/', Ann0View.as_view()),
+    path('projects/nyanko/', NyankoView.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,26 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class ANNView(TemplateView):
+class ProjectsView(TemplateView):
+    template_name = 'projects.html'
+
+class AboutusView(TemplateView):
+    template_name = 'aboutus.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+class TimelineView(TemplateView):
+    template_name = 'timeline.html'
+
+class AnnView(TemplateView):
     template_name = 'ann.html'
+
+class WankoView(TemplateView):
+    template_name = 'wanko.html'
+
+class Ann0View(TemplateView):
+    template_name = 'ann0.html'
+    
+class NyankoView(TemplateView):
+    template_name = 'nyanko.html'
