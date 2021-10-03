b21db48544f447df9531e83a093b820881fab3c2
daikoshg@gmail.com <daikoshg@gmail.com>
2021-10-03 17:04:15 +0900

    base.html

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ANNView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('ann/', ANNView.as_view()),
 ]
