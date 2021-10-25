c95af27007a36eafacae9db174aa6dc42ac23455
daikoshg@gmail.com <daikoshg@gmail.com>
2021-10-25 20:42:00 +0900

    timeline

--- pj_rikakyon/settings.py
+++ pj_rikakyon/settings.py
@@ -116,10 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
